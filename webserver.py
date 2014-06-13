6c23ca9d67abe68265d11cd0edaec61ababb970f
Sam Sandberg <sam.sandberg@gmail.com>
2014-06-13 09:29:56 -0400

    Caching route for 10 seconds

--- webserver.py
+++ webserver.py
@@ -4,23 +4,37 @@ import calendar
 import datetime
 
 from flask import Flask, jsonify
+from flask.ext.cache import Cache
 from scraper import scrape
 
 
 app = Flask(__name__)
-app.debug = True
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+# app.debug = True
+
+
+def now_timestamp():
+    now_tt = datetime.datetime.utctimetuple(datetime.datetime.utcnow())
+    return calendar.timegm(now_tt)
+
+
+# TODO: Cache this function or the /api route? Going with the route for now
+# @cache.cached(timeout=10, key_prefix='data')
+def get_data():
+    data = scrape()
+    data['last_updated'] = now_timestamp()
+    return data
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World (Cup)!'
+
 
 @app.route("/api")
+@cache.cached(timeout=10)
 def api():
-    data = scrape()
-    now = datetime.datetime.utctimetuple(datetime.datetime.utcnow())
-    data['last_updated'] = calendar.timegm(now)
-    return jsonify(data)
+    return jsonify(get_data())
 
 
 if __name__ == "__main__":
